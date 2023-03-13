3866c0fb13870e30865cf6fe5a8a656f64c86a08
technexx <48839830+technexx@users.noreply.github.com>
2023-03-13 12:59:05 -0700

    rows + subrows added to status bar column

--- app/src/main/java/the/head/that/feeds/MainActivity.kt
+++ app/src/main/java/the/head/that/feeds/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,8 +21,10 @@ import the.head.that.feeds.ui.theme.TheHeadThatFeedsTheme
 
 //Todo: All humanity is under control of AI. It controls all media access. Progress is made by poking holes/sapping power/exploiting blind spots.
 //Todo: Can locate other "off the grid" humans to help.
+
 //Todo: As player AI evolves, it gains new abilities.
-//Todo: As enemy AI gets closer to locating player, it gains new abilities.
+    //Todo: Requires more power as it grows.
+//Todo: As enemy AI gets closer to locating player, it gains new abilities. Maybe relate to its animal.
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,20 +47,50 @@ fun FullGameScreen() {
     val screenHeight = configuration.screenHeightDp
 
     Column() {
-        GameStatusBar(screenHeight / 4)
+        GameStatusBarColumn(screenHeight / 4)
+        Divider(thickness = 2.dp, color = Color.White)
         GameBody  (screenHeight / 2)
+        Divider(thickness = 2.dp, color = Color.White)
         GameInteraction(screenHeight / 4)
     }
 }
 
+//Columns lay down their children vertically, so of course our rows will be stacked.
 @Composable
-fun GameStatusBar(height: Int) {
+fun GameStatusBarColumn(height: Int) {
     Column(modifier = Modifier
-        .fillMaxWidth()
         .height(height.dp)
-        .background(colorResource(id = R.color.blue))
+        .background(colorResource(id = R.color.black_denim))
     ) {
+        GameStatusBarMainRow(backgroundColor = R.color.android_blue)
     }
+}
+
+@Composable
+fun GameStatusBarMainRow(backgroundColor: Int) {
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .background(colorResource(id = backgroundColor))
+    ) {
+        GameStatsBarSubRows(backgroundColor = R.color.android_yellow)
+        GameStatsBarSubRows(backgroundColor = R.color.android_magenta)
+        GameStatsBarSubRows(backgroundColor = R.color.android_green)
+    }
+}
+
+@Composable
+fun GameStatsBarSubRows(backgroundColor: Int) {
+    Row (modifier = Modifier
+        .fillMaxHeight()
+        .background(colorResource(id = backgroundColor))
+        .width(120.dp)) {
+
+    }
+}
+
+@Composable
+fun StatusBarTexts(textColor: Color) {
+    Text(text = "Resistance AI", color = textColor)
 }
 
 @Composable
@@ -64,7 +98,7 @@ fun GameBody(height: Int) {
     Column(modifier = Modifier
         .fillMaxWidth()
         .height(height.dp)
-        .background(colorResource(id = R.color.white))
+        .background(colorResource(id = R.color.onyx))
     ) {
     }
 }
@@ -74,7 +108,7 @@ fun GameInteraction(height: Int) {
     Column(modifier = Modifier
         .fillMaxWidth()
         .height(height.dp)
-        .background(colorResource(id = R.color.android_yellow))
+        .background(colorResource(id = R.color.darker_grey))
     ) {
     }
 }
